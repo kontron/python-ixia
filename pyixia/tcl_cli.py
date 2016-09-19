4a4383f3687609cd691124095d3b080050e03820
Jochen Baltes <jochen.baltes@kontron.com>
2016-09-19 11:45:19 +0200

    add socket error exception in tcl_cli.py
    Signed-off-by: Jochen Baltes <jochen.baltes@kontron.com>

--- pyixia/tcl_cli.py
+++ pyixia/tcl_cli.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import readline
 from optparse import OptionParser
+import socket
 
 from pyixia.tclproto import TclClient, TclError
 
@@ -26,27 +27,30 @@ def main():
 
     host = args[0]
 
-    tcl = TclClient(host)
-    tcl.connect()
-    if options.autoconnect:
-        print tcl.call('ixConnectToChassis %s', host)[1]
-
-    print "Enter command to send. Quit with 'q'."
     try:
-        while True:
-            cmd = raw_input('=> ')
-            if cmd == 'q':
-                break
-            if len(cmd) > 0:
-                try:
-                    (res, io) = tcl.call(cmd)
-                    print res
-                    if io is not None:
-                        print io.replace('\r','\n')
-                except TclError, e:
-                    print 'ERROR: %s' % e.result
-    except EOFError:
-        print 'exitting..'
+        tcl = TclClient(host)
+        tcl.connect()
+        if options.autoconnect:
+            print tcl.call('ixConnectToChassis %s', host)[1]
+    
+        print "Enter command to send. Quit with 'q'."
+        try:
+            while True:
+                cmd = raw_input('=> ')
+                if cmd == 'q':
+                    break
+                if len(cmd) > 0:
+                    try:
+                        (res, io) = tcl.call(cmd)
+                        print res
+                        if io is not None:
+                            print io.replace('\r','\n')
+                    except TclError, e:
+                        print 'ERROR: %s' % e.result
+        except EOFError:
+            print 'exitting..'
+    except socket.error as e:
+        print e.strerror
 
 if __name__ == '__main__':
     main()
