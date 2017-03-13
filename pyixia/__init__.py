b2c1d69da195cb4571ab516128c0eb0d885835b7
Yoram Shamir <yoram-s@qualisystems.com>
2017-03-13 15:04:30 +0200

    add requirements file

--- pyixia/__init__.py
+++ pyixia/__init__.py
@@ -19,7 +19,7 @@ from tclproto import TclClient
 from ixapi import _MetaIxTclApi, TclMember, FLAG_RDONLY
 from ixapi import IxTclHalApi, IxTclHalError
 
-__version__ = '0.9.4.0'
+__version__ = '0.9.4.1'
 
 log = logging.getLogger(__name__)
 
